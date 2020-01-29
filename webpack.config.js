642b801f08bdd34fbc0bc2ed4f03bbf23311e24d
Necritto <onetopyr@mail.ru>
2020-01-29 19:30:20 +0300

    Feature: connected webpack [ref:no-ref]

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          }, {
+            loader: 'postcss-loader',
+            options: { config: { path: './postcss.config.js' } }
+          }, {
+            loader: 'sass-loader',
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new CleanWebpackPlugin(),
+  ],
+};
