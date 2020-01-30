a916c033ad1758650f6baaa8ccf8538b4ce5c0cf
Necritto <onetopyr@mail.ru>
2020-01-30 21:24:19 +0300

    Feature: player added [ref:no-ref]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './assets/scss/index.scss';
-import './js/key';
-import './js/engine';
-import './js/graph';
 import './js/arkanoid';
--- src/js/arkanoid.js
+++ src/js/arkanoid.js
@@ -1,32 +1,12 @@
-import * as key from './key';
 import * as engine from './engine';
 import * as graph from './graph';
-
-let platformWidth = 100;
-const platformHeight = 10;
-let platformX = (graph.width / 2) - (platformWidth / 2);
-const platformY = graph.height - 20;
-let platformColor = 'blue';
+import { player } from './player';
 
 graph.clearAll();
 
 engine.startGame(function () {
   graph.fillAll('#add8e6');
-  graph.drawRect(platformX, platformY, platformWidth, platformHeight, platformColor);
 
-  if (key.isKeyDown('LEFT')) {
-    platformX--;
-  }
-
-  if (key.isKeyDown('RIGHT')) {
-    platformX++;
-  }
-
-  if (key.isKeyDown('A')) {
-    platformX--;
-  }
-
-  if (key.isKeyDown('D')) {
-    platformX++;
-  }
+  player.move();
+  player.draw();
 });
